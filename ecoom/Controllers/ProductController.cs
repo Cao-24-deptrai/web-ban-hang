9bf12fc48aef2fb4d22db0f251cf14593caaa58b
caonguyeanh <caonguyenanh24@gmial.com>
2025-11-22 19:01:57 +0700

    update code

---
+++ ecoom/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using ecoom.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ecoom.Controllers
+{
+    public class ProductController : Controller
+    {
+        qlbhEntities db = new qlbhEntities();
+        // GET: Product
+        public ActionResult IndexCustomer(decimal? _min, decimal? _max)
+        {
+            var proFilter = db.Products.AsQueryable();
+            
+            if (_min.HasValue)
+                proFilter = proFilter.Where(s => s.Price >= _min.Value);
+            if (_max.HasValue)
+                proFilter = proFilter.Where(s => s.Price <= _max.Value);
+            return View(proFilter.ToList());
+        }
+        public ActionResult Index(decimal? _min, decimal? _max)
+        {
+            var proFilter = db.Products.AsQueryable();
+            if (_min.HasValue)
+                proFilter = proFilter.Where(s => s.Price >= _min.Value);
+            if (_max.HasValue)
+                proFilter = proFilter.Where(s => s.Price <= _max.Value);
+            return View(proFilter.ToList());
+        }
+    }
+}
